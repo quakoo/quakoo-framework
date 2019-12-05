1fb549c9d9b3e062c9577bebbdf554721f07dece
lihao <haolibj7741@sohu-inc.com>
2019-12-05 14:26:06 +0800

    Signed-off-by: lihao <haolibj7741@sohu-inc.com>

--- quakoo-base-framework/src/main/java/com/quakoo/baseFramework/transform/ListTransformUtils.java
+++ quakoo-base-framework/src/main/java/com/quakoo/baseFramework/transform/ListTransformUtils.java
@@ -1,12 +1,13 @@
 package com.quakoo.baseFramework.transform;
 
+import java.util.Collection;
 import java.util.List;
 
 import com.google.common.collect.Lists;
 
 public class ListTransformUtils {
 
-	public static <S, T> List<T> transformedList(List<S> list, ListTransformer<S, T> trans) {
+	public static <S, T> List<T> transformedList(Collection<S> list, ListTransformer<S, T> trans) {
 	     List<T> res = Lists.newArrayList();
 	     for(S one : list) {
 	    	 T t = trans.transform(one);
